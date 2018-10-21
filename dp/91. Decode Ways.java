323139425cddac04c2ba80e071bb1fff6085578c
nowherelemon <lemonnowhere@gmail.com>
2018-10-20 21:24:08 -0700

    Create 91. Decode Ways.java

---
+++ dp/91. Decode Ways.java
@@ -0,0 +1,44 @@
+/*
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+*/
+
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        
+        for (int i = 1; i < s.length(); i++) {
+            String one = s.substring(i, i + 1);
+            String two = s.substring(i - 1, i + 1);
+            
+            if (Integer.parseInt(one) != 0) {
+                dp[i + 1] += dp[i];
+            }
+            if (Integer.parseInt(two) <= 26 && Integer.parseInt(two) >= 10) {
+                dp[i + 1] += dp[i - 1];
+            }
+        }
+        return dp[s.length()];
+        
+    }
+}
