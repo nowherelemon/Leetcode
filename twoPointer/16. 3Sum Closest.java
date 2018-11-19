37ff4849d639042ce777fc077bc5d496221429dc
nowherelemon <lemonnowhere@gmail.com>
2018-11-19 14:18:17 -0800

    Create 16. 3Sum Closest.java

---
+++ twoPointer/16. 3Sum Closest.java
@@ -0,0 +1,37 @@
+/*
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+Example:
+
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] res = new int[1];
+        int dif = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[r] + nums[i];
+                if (Math.abs(target - sum) < dif) {
+                    dif = Math.abs(target - sum);
+                    res[0] = sum;
+                }
+                if (sum < target) {
+                    l++;
+                } else if (sum > target) {
+                    r--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+        return res[0];
+    }
+}
